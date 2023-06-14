683dae7d1127cc533114d094967600bd837165e8
“willy <[email protected]>
2023-06-14 15:02:03 +0300

    corsmiddleware set up

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    
+
 )
 
 app.include_router(users_router, prefix="/users", tags=["users"])
