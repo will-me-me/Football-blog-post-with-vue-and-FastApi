d1ee92818ba70b7d2bacac3fd5f506889c6cba02
“willy <[email protected]>
2023-06-21 12:55:01 +0300

    added the add blog post dialog

--- src/components/LandingAppBar.vue
+++ src/components/LandingAppBar.vue
@@ -1,17 +1,94 @@
 <template>
   <div>
-    <v-app-bar app color="" id="appbar" dense outlined flat>
+    <v-app-bar color="#2962FF" id="appbar" dense outlined dark app fixed>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <!-- <v-app-bar-icon>
         <v-icon x-large>mdi-soccer</v-icon>
       </v-app-bar-icon> -->
-      <div>
-        <v-btn text to="/" class="ma-6">Home</v-btn>
-        <v-btn text @click="ToBlogsPath" class="ma-6">Blogs</v-btn>
-        <v-btn text class="ma-6" @click="ToLoginPath">Login</v-btn>
-        <v-btn text class="ma-6" @click="ToCreateAccount">Sign in</v-btn>
-      </div>
+      <!-- <div> -->
+      <v-btn rounded text to="/" class="ma-6">Home</v-btn>
+      <v-btn rounded text @click="ToBlogsPath" class="ma-6">Blogs</v-btn>
+      <v-btn rounded text class="ma-6" @click="ToLoginPath">Login</v-btn>
+      <v-btn rounded text class="ma-6" @click="ToCreateAccount">Sign in</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn rounded text class="ma-6" @click="OpenblogDialog">
+        <v-icon small left> mdi-plus-circle </v-icon>
+        Add Blog
+      </v-btn>
+      <!-- </div> -->
     </v-app-bar>
+    <v-dialog persistent v-model="Addblogdialog" width="500">
+      <v-card width="600" outlined>
+        <v-card-title class="d-flex justify-center">
+          <span class="caption overline">Add Blog</span>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-text-field
+            color="deep-purple accent-4"
+            prepend-icon="mdi-book-open-page-variant"
+            name="Title"
+            label="Title"
+            type="text"
+          ></v-text-field>
+          <v-textarea
+            color="deep-purple accent-4"
+            auto-grow
+            label="Content"
+            rows="1"
+            outlined
+            prepend-icon="mdi-book-open-page-variant"
+          ></v-textarea>
+          <v-file-input
+            color="deep-purple accent-4"
+            prepend-icon="mdi-camera"
+            name="pic"
+            label="media"
+            type="file"
+            chips
+            filled
+            multiple
+            clearable
+            clear-icon="mdi-close-circle-outline"
+          >
+            <template v-slot:selection="{ text }">
+              <v-chip small label color="primary" draggable dark>
+                {{ text }}
+              </v-chip>
+            </template>
+          </v-file-input>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+            color="blue darken-1"
+            text
+            outlined
+            @click="Addblogdialog = false"
+            >Cancel</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            outlined
+            @click="Addblogdialog = false"
+            >Add Blog</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+
+      <v-overlay :value="overlay">
+        <v-progress-circular
+          indeterminate
+          size="94"
+          color="teal"
+          width="7"
+          height="7"
+        ></v-progress-circular>
+      </v-overlay>
+    </v-dialog>
+
     <v-progress-linear
       v-if="showProgressBar"
       color="teal"
@@ -73,7 +150,10 @@
 // import { set } from 'vue/types/umd';
 /* eslint-disable */
 export default {
+  components: {},
   data: () => ({
+    overlay: false,
+    Addblogdialog: false,
     showProgressBar: false,
     showProgressBarreverse: false,
     value: 0,
@@ -101,6 +181,15 @@ export default {
           this.$router.push("/login");
         }, 1500);
       }
+    },
+
+    OpenblogDialog() {
+      this.Addblogdialog = true;
+      this.overlay = true;
+      setTimeout(() => {
+        this.overlay = false;
+        // this.Addblogdialog = false;
+      }, 1500);
     },
 
     ToCreateAccount() {
